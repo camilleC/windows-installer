712524daa28c3ea9244a5820529fe48056291c40
Clark Rinker <cgrinker@gmail.com>
2012-06-25 10:30:23 -0700

    Currently Working on utility functions that manage searching either the file system or the registry to detect local versions of packages. Managers will probably add stuff to their ini file (or another ini) file and the utility will use these functions to detect versions.
    It's rather exception happy. Also I don't know what PEP8 is
    
    Maybe wait to merge up the tree
    Signed-off-by: Clark Rinker <cgrinker@gmail.com>

---
+++ local_utils.py
@@ -0,0 +1,40 @@
+#Ultils to scan local files for Version Information
+#@Clark Rinker, June 2012
+#@Western Washington University
+
+import re
+testPath="C:\\Python27\\README.txt"
+vRegex="[0-9]+(?:\.[0-9]+)*"
+
+
+class AmbiguousMatchError(Exception):
+    def __init__(self,value):
+        self.value=value
+    def __str__(self):
+        return repr(self.value)
+
+
+class NoMatchError(Exception):
+    def __init__(self,value):
+        self.value=value
+    def __str__(self):
+        return repr(self.value)
+
+   
+def findVersionInFile(fileString,pattern):
+        s=open(fileString()).read()
+        matches=re.findall(pattern,s)
+        if len(matches)==0:
+            raise NoMatchError
+
+        if len(matches>1):
+            raise AmbiguousMatchError 
+        else:
+            return matches[0]
+        
+
+
+
+
+#def test():
+
